7f27cb86bf65d50ec648bbbde5d0563831dd3beb
Terrence John Garcia Pacursa <zerothreethirtyone@yahoo.com>
2016-12-20 07:57:33 +0800

    Create if_play.c

---
+++ if_play.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+	int main()
+	{
+		int number;
+		
+		printf("ENTER A NUMBER\n");
+		scanf("%d", &number);
+		if (number%3==0)
+			{
+				printf("Ed ");		
+			}
+		if(number%5==0)
+			{
+				printf("pogi\n");
+				
+			}
+	}
