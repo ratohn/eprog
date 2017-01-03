4695c2ffaef7c948f90ad54a31f094ac149ef37e
Terrence John Garcia Pacursa <zerothreethirtyone@yahoo.com>
2017-01-03 09:01:18 +0800

    Update odd.c

--- odd.c
+++ odd.c
@@ -1,11 +1,9 @@
-#include <stdio.h>
-int main (){
-	int i;
-	printf("Odd numbers up to 100 are:\n");
-	i=1;
-	do{
-		printf("%d\n",i);
-		i=i+2;
+#include<stdio.h>
+int main(){
+	
+	int i=0;
+	while(i<=100){i++;
+		  if(i%2==1)
+		  printf("%d\n",i);
 	}
-	while(i<=100);
 }
